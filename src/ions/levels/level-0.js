b3703b9c934b72c609e97c0a222432961815ec97
Dominik Hautau <dominikhautau@gmail.com>
2022-02-02 18:14:32 +0100

    style: rotated the player to another degree

--- src/ions/levels/level-0.js
+++ src/ions/levels/level-0.js
@@ -6,7 +6,7 @@ createLevel({
 		position: [-10, 4.5, 0],
 		args: [0.5, 1.5, 4],
 		color: colors.primaryLight,
-		rotation: [0, 0, -45],
+		rotation: [0, 0, -0.785398],
 		metalness: "0.5",
 		roughness: "0",
 	},
