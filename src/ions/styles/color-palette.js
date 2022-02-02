b3b673bf7e396ec4c7ac2c65c85b746e0f14247a
Dominik Hautau <dominikhautau@gmail.com>
2022-02-02 15:55:55 +0100

    style: reduced color-palette to greyscale for a less opinionated styling

--- src/ions/styles/color-palette.js
+++ src/ions/styles/color-palette.js
@@ -3,13 +3,10 @@ import { css } from "@emotion/react";
 export const colors = {
 	black: "#000",
 	white: "#fff",
-	primaryLight: "#757ce8",
-	primaryMain: "#3f50b5",
-	primaryDark: "#002884",
+	primaryLight: "#b2bec3",
+	primaryMain: "#636e72",
+	primaryDark: "#2d3436",
 	primaryContrastText: "#fff",
-	secondaryLight: "#ff7961",
-	secondaryMain: "#f44336",
-	secondaryDark: "#ba000d",
 	secondaryContrastText: "#000",
 };
 
@@ -23,9 +20,6 @@ export const colorPalette = css`
 		--primary-main: ${colors.primaryMain};
 		--primary-dark: ${colors.primaryDark};
 		--primary-contrast-text: ${colors.primaryContrastText};
-		--secondary-light: ${colors.secondaryLight};
-		--secondary-main: ${colors.secondaryMain};
-		--secondary-dark: ${colors.secondaryDark};
 		--secondary-contrast-text: ${colors.secondaryContrastText};
 	}
 `;
