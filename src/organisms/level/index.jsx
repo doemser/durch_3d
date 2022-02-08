79f35ae49110a0aab2f8a8713b9b292c9811098d
Dominik Hautau <93415777+doemser@users.noreply.github.com>
2022-02-08 15:52:07 +0100

    feat: added statistics about how many moves player needed to complete (#14)

--- src/organisms/level/index.jsx
+++ src/organisms/level/index.jsx
@@ -7,11 +7,9 @@ import Lights from "../../molecules/lights";
 import Map from "../../molecules/map";
 import Goal from "../../atoms/goal";
 import useStore from "../../ions/store";
-import Texts from "../../molecules/texts";
 
 const Level = () => {
 	const level = useStore(state => state.level);
-	const gameState = useStore(state => state.gameState);
 	const build = useStore(state => state.build);
 	const currentLevel = levels[Number.parseInt(level)];
 
