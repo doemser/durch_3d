69618b10dd70cbda3279d35e395d2aa849faff80
Dominik Hautau <dominikhautau@gmail.com>
2022-02-09 16:41:25 +0100

    feat: put more than one rotating box into a level

--- src/ions/levels/index.js
+++ src/ions/levels/index.js
@@ -3,6 +3,7 @@ import "./level-1";
 import "./level-2";
 import "./level-3";
 import "./level-4";
+import "./level-5";
 import levels from "./levels";
 export const { length: levelCount } = levels;
 export default levels;
