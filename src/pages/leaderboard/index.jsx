c90c8d7b45a1c645a721c32949987d962f270b97
Dominik Hautau <93415777+doemser@users.noreply.github.com>
2022-02-20 03:24:59 +0100

    Fix highscoring (#24)
    * chore: fixed false logic in highscoring added mui
    
    * feat: added sorted leaderboard
    
    * refactor: sort highscores while fetching now

--- src/pages/leaderboard/index.jsx
+++ src/pages/leaderboard/index.jsx
@@ -4,6 +4,13 @@ import StyledImage from "../../atoms/image";
 import useStore from "../../ions/store";
 import Layout from "../../organisms/layout";
 import useTransientSession from "../../ions/hooks/use-transient-session";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const Page = () => {
 	useTransientSession();
@@ -25,17 +32,36 @@ const Page = () => {
 				/>
 			</Head>
 			<h1>Leaderboard</h1>
-			<ul>
-				{highscores.map(highscore => {
-					return (
-						<li key={highscore._id}>
-							<StyledImage width={35} height={35} src={highscore.image} />
-							{highscore.name} - score: {highscore.score} - moves: {highscore.moves} -
-							deaths: {highscore.deaths}
-						</li>
-					);
-				})}
-			</ul>
+			<TableContainer component={Paper}>
+				<Table aria-label="durch leaderboard">
+					<TableHead>
+						<TableRow>
+							<TableCell>#</TableCell>
+							<TableCell align="right" />
+							<TableCell align="right">Name</TableCell>
+							<TableCell align="right">Score</TableCell>
+							<TableCell align="right">Moves</TableCell>
+							<TableCell align="right">Deaths</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{highscores.map((highscore, index) => (
+							<TableRow key={highscore.user}>
+								<TableCell component="th" scope="row">
+									{index + 1}
+								</TableCell>
+								<TableCell align="left">
+									<StyledImage width={35} height={35} src={highscore.image} />
+								</TableCell>
+								<TableCell align="right">{highscore.name}</TableCell>
+								<TableCell align="right">{highscore.score}</TableCell>
+								<TableCell align="right">{highscore.moves}</TableCell>
+								<TableCell align="right">{highscore.deaths}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
 		</Layout>
 	);
 };
