ed5e1b0316bf7faee389cc279eaefbe02e43d8af
Dominik Hautau <dominikhautau@gmail.com>
2022-02-20 17:15:14 +0100

    feat: 2 level leaderboard sorting

--- src/pages/leaderboard/index.jsx
+++ src/pages/leaderboard/index.jsx
@@ -17,8 +17,7 @@ const Page = () => {
 	const highscores = useStore(state => state.highscores);
 
 	useEffect(() => {
-		const getHighscores = useStore.getState().getHighscores;
-		getHighscores();
+		useStore.getState().getHighscores();
 	}, []);
 
 	return (
@@ -31,17 +30,17 @@ const Page = () => {
 					content="a little game about shooting through"
 				/>
 			</Head>
-			<h1>Leaderboard</h1>
+			<h1>DURCH</h1>
 			<TableContainer component={Paper}>
 				<Table aria-label="durch leaderboard">
 					<TableHead>
 						<TableRow>
 							<TableCell>#</TableCell>
-							<TableCell align="right" />
-							<TableCell align="right">Name</TableCell>
-							<TableCell align="right">Score</TableCell>
-							<TableCell align="right">Moves</TableCell>
-							<TableCell align="right">Deaths</TableCell>
+							<TableCell align="center" />
+							<TableCell align="center">Name</TableCell>
+							<TableCell align="center">Score</TableCell>
+							<TableCell align="center">Moves</TableCell>
+							<TableCell align="center">Deaths</TableCell>
 						</TableRow>
 					</TableHead>
 					<TableBody>
@@ -50,13 +49,15 @@ const Page = () => {
 								<TableCell component="th" scope="row">
 									{index + 1}
 								</TableCell>
-								<TableCell align="left">
+								<TableCell align="center">
 									<StyledImage width={35} height={35} src={highscore.image} />
 								</TableCell>
-								<TableCell align="right">{highscore.name}</TableCell>
-								<TableCell align="right">{highscore.score}</TableCell>
-								<TableCell align="right">{highscore.moves}</TableCell>
-								<TableCell align="right">{highscore.deaths}</TableCell>
+								<TableCell align="center" align="center">
+									{highscore.name}
+								</TableCell>
+								<TableCell align="center">{highscore.score}</TableCell>
+								<TableCell align="center">{highscore.moves}</TableCell>
+								<TableCell align="center">{highscore.deaths}</TableCell>
 							</TableRow>
 						))}
 					</TableBody>
