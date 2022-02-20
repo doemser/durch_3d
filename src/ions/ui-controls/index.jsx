84192dd37d965b115142409a64fde0e28f92157b
Dominik Hautau <dominikhautau@gmail.com>
2022-02-20 05:17:29 +0100

    refactor: ui-controls

--- src/ions/ui-controls/index.jsx
+++ src/ions/ui-controls/index.jsx
@@ -13,18 +13,14 @@ const UiControls = () => {
 			const setOverallStats = useStore.getState().setOverallStats;
 			const resetMoves = useStore.getState().resetMoves;
 
-			if (code === "Space") {
-				if (gameState === "waiting" || gameState === "lose") {
-					setGameState("running");
-				}
+			if (code === "Space" && (gameState === "waiting" || gameState === "lose")) {
+				setGameState("running");
 			}
-			if (code === "Enter") {
-				if (gameState === "win") {
-					router.push(`/play/${parseInt(level) + 1}`);
-					setGameState("waiting");
-					setOverallStats(gameState);
-					resetMoves();
-				}
+			if (code === "Enter" && gameState === "win") {
+				router.push(`/play/${parseInt(level) + 1}`);
+				setGameState("waiting");
+				setOverallStats(gameState);
+				resetMoves();
 			}
 		};
 
